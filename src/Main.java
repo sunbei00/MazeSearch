b050c39318700c5796224f6e7e115e048b67e6df
sunbei00 <sunbei00@naver.com>
2023-05-07 03:54:28 +0900

    미로 지도 파일 읽기 기능 추가. 현재 미로, 올바른 미로 img 출력 기능 추가.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Model model = new Model("./Maze/Maze1.txt", "./result.bmp");
+        model.fileRead();
+        model.buildOur();
+
+        model.setWritePath("GroundTruth.bmp");
+        model.ImgWrite(Model.ImgOutput.GroundTruth);
+        model.setWritePath("Our.bmp");
+        model.ImgWrite(Model.ImgOutput.Our);
     }
 }
---
+++ src/Model.java
@@ -0,0 +1,150 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Iterator;
+
+
+
+public class Model {
+    static final int AIR = '0';
+    static final int WALL = '1';
+    static final int UNKNOWN = '2';
+    static final int BRANCH = '3';
+    static final int PLAYER = '4';
+
+    enum ImgOutput{
+        GroundTruth,
+        Our
+    }
+
+    private String readPath = null;
+    private String writePath = null;
+    private int row = -1;
+    private int col = -1;
+    public ArrayList<ArrayList<Integer>> groundTruth = null;
+    // public으로 할지, private으로 할지는 고민해보기
+    public ArrayList<ArrayList<Integer>> our = null;
+
+    public Model() {}
+    public Model(String readPath){
+        this.readPath = readPath;
+    }
+    public Model(String readPath, String writePath){
+        this.readPath = readPath;
+        this.writePath = writePath;
+    }
+    public void setReadPath(String readPath){
+        this.readPath = readPath;
+    }
+    public void setWritePath(String writePath){
+        this.writePath = writePath;
+    }
+
+    public void fileRead() {
+        try{
+            if(this.readPath == null)
+                throw new IOException("Need to set variable 'readPath'");
+            FileReader fileReader = new FileReader(this.readPath);
+
+            int count=0;
+            int ch;
+            this.groundTruth = new ArrayList<ArrayList<Integer>>();
+            this.groundTruth.add(count,new ArrayList<Integer>()); // 최소 1개의 row가 있다고 가정.
+            while((ch = fileReader.read()) != -1){
+                if(ch == '\n'){
+                    count++;
+                    this.groundTruth.add(count,new ArrayList<Integer>());
+                }
+                else if(ch == AIR || ch == WALL)
+                    this.groundTruth.get(count).add(ch);
+                else if( ch > 32  && ch != 127) // 제어 문자, 빈 문자, 0,1 이외의 값이 들어오면 오류 처리
+                    throw new IOException("File is Error(Error Character) : " +  (char)ch);
+            }
+            this.row = this.groundTruth.size();
+            this.col = this.groundTruth.get(0).size();
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void buildOur(){
+        try{
+            if(readPath == null)
+                throw new IOException("Need to run 'fileRead'");
+            this.our = new ArrayList<ArrayList<Integer>>();
+
+            for(int i=0;i<row;i++){
+                our.add(i,new ArrayList<Integer>());
+                for(int j=0;j<col;j++)
+                    our.get(i).add(UNKNOWN);
+            }
+
+            our.get(0).set(1,BRANCH);
+
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void ImgWrite(ImgOutput imgOutput){
+        try{
+            if(this.writePath == null)
+                throw new IOException("Need to set variable 'writePath'");
+            if(groundTruth == null && imgOutput == ImgOutput.GroundTruth)
+                throw new IOException("Need to run 'fileRead'");
+            if(our == null && imgOutput == ImgOutput.Our)
+                throw new IOException("Need to run 'buildOur'");
+
+            BufferedImage img = new BufferedImage(col, row, BufferedImage.TYPE_3BYTE_BGR);
+            File file = new File(writePath);
+
+            int count=0;
+            int element;
+            Iterator<ArrayList<Integer>> rowIT = null;
+            Iterator<Integer> colIT = null;
+
+            switch (imgOutput){
+                case GroundTruth:
+                    rowIT = groundTruth.iterator();
+                    break;
+                case Our:
+                    rowIT = our.iterator();
+                    break;
+            }
+
+            while(rowIT.hasNext()){
+                colIT = rowIT.next().iterator();
+                while(colIT.hasNext()){
+                    element = colIT.next();
+                    switch (element){ // xx RR GG BB
+                        case AIR:
+                            img.setRGB(count%col,count/col,0x00ffffff); // white
+                            break;
+                        case WALL:
+                            img.setRGB(count%col,count/col,0x00000000); // black
+                            break;
+                        case BRANCH:
+                            img.setRGB(count%col,count/col,0x0066ff66); // Green
+                            break;
+                        case UNKNOWN:
+                            img.setRGB(count%col,count/col,0x00444444); // Gray
+                            break;
+                        case PLAYER:
+                            img.setRGB(count%col,count/col,0x00ff0000); // RED
+                            break;
+                    }
+                    count++;
+                }
+            }
+
+            ImageIO.write(img, "bmp", file);
+
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
